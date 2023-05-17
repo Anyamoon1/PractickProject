761811c5bad669b184a1dc0b6e64c1ac0e3309cc
Кулагина Анна Петровна <gr604_kuape@ttit.local>
2023-05-17 16:10:22 +0700

    Функция изменения статуса

---
+++ MethodChangeStatus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MIAPO
+{
+    public class MethodChangeStatus
+    {
+        public static List<MyTable> ChangeStatus(MyTable obj, List<MyTable> list)
+        {
+            if (obj == null)
+            {
+                return new List<MyTable>();
+            }
+            if (obj.Status == "Активный")
+            {
+                obj.Status = "Не активный";
+                int idx = list.IndexOf(obj);
+                list.Remove(obj);
+                list.Insert(idx, new MyTable(obj.Id, obj.Surname, obj.NameEmployee, obj.Patronymic, obj.NumberPhone, obj.Status));
+                return list;
+            }
+            else
+            {
+                return new List<MyTable>();
+            }
+        }
+    }
+}
